2101c15e7ce01f62bb8cbc96c4c454e1835c5587
miladsoft <miladsoft@users.noreply.github.com>
2024-04-14 12:28:46 +0000

    deploy: ee6903f9ffafda3dd7f6478a21a420140d1d7170

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Function to fetch JSON data
     async function fetchData() {
         try {
             const response = await fetch("data/data.json");
@@ -10,46 +9,32 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    // Function to update UI with the latest data
     async function updateUI() {
-        // Update date and time
-        var currentDate = new Date();
-        var dateTimeString = currentDate.toLocaleString();
+        const currentDate = new Date();
+        const dateTimeString = currentDate.toLocaleString();
         document.getElementById("datetime").innerHTML = dateTimeString;
 
-        // Update price and datetime from JSON data
         const data = await fetchData();
         if (data && data.length > 0) {
             const latestData = data[data.length - 1];
             const { price, datetime } = latestData;
 
-            // Update price and datetime in the UI
-            document.getElementById("irr").textContent = price + " IRR";
-            document.getElementById("updatedate").textContent = "Last update: " + new Date(datetime).toLocaleString();
+            document.getElementById("irr").textContent = `${price} IRR`;
+            document.getElementById("updatedate").textContent = `Last update: ${new Date(datetime).toLocaleString()}`;
         }
     }
 
-    // Call updateUI initially to load data on page load
     updateUI();
 
-    // Call updateUI every minute (60000 milliseconds) to keep data and time updated
     setInterval(updateUI, 60000);
 
     function updateDateTime() {
-        // Get the current date and time
-        var currentDate = new Date();
-        
-        // Format the date and time
-        var dateTimeString = currentDate.toLocaleString();
-        
-        // Display the date and time in the div with id="datetime"
+        const currentDate = new Date();
+        const dateTimeString = currentDate.toLocaleString();
         document.getElementById("datetime").innerHTML = dateTimeString;
     }
-    
-    // Call the function once to display the date and time immediately
+
     updateDateTime();
-    
-    // Call the function every second to update the date and time in real-time
+
     setInterval(updateDateTime, 1000);
-    
 });
